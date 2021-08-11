b9762eabb957f5e785c916cad9e5e71ccaf24b0d
Jacky Chu <jacky.ju@gmail.com>
2021-08-10 18:29:09 -0700

    Change translate library from Google Translate to OpenCC (OpenCC4j)

--- src/main/java/app/jackychu/jysrttools/ui/TranslateProgressDialog.java
+++ src/main/java/app/jackychu/jysrttools/ui/TranslateProgressDialog.java
@@ -1,17 +1,12 @@
 package app.jackychu.jysrttools.ui;
 
-import app.jackychu.api.simplegoogletranslate.Language;
-import app.jackychu.api.simplegoogletranslate.SimpleGoogleTranslate;
 import app.jackychu.jysrttools.JyDraft;
 import app.jackychu.jysrttools.JyUtils;
-import app.jackychu.jysrttools.exception.JySrtToolsException;
+import com.github.houbb.opencc4j.util.ZhConverterUtil;
 
 import javax.swing.*;
 import java.awt.*;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class TranslateProgressDialog extends JDialog {
@@ -19,7 +14,6 @@ public class TranslateProgressDialog extends JDialog {
     private final JProgressBar progressBar;
     private final JLabel label;
     private final JButton btnOk;
-    private final SimpleGoogleTranslate translate = new SimpleGoogleTranslate();
 
     public TranslateProgressDialog(JFrame parent, boolean modal) {
         super(parent, modal);
@@ -56,50 +50,19 @@ public class TranslateProgressDialog extends JDialog {
             @Override
             protected Void doInBackground() {
                 int i = 0;
-                int limit = 100;
                 int percentage;
 
                 try {
                     Map<String, String> texts = draft.getDraftTexts();
-
-                    // Split large texts and every 100 texts concat into one string with delimiter |||
-                    List<String> str = new ArrayList<>();
-                    StringBuilder sb = new StringBuilder();
-                    int start = 0;
-                    for (String text : texts.values()) {
-                        if (start < limit - 1) {
-                            sb.append(text).append("|||");
-                            start++;
-                        } else {
-                            start = 0;
-                            sb.append(text);
-                            str.add(sb.toString());
-                            sb = new StringBuilder();
-                        }
-                    }
-                    str.add(sb.toString());
-
-                    // Translate new text string and split it out back to normal text string
-                    List<String> newStr = new ArrayList<>();
-                    for (String s : str) {
-                        String ns = translate.doTranslate(Language.zh_cn, Language.zh_tw, s);
-                        // mysterious issue. Sometime the delimiter ||| will becomes | || after translated
-                        ns = ns.replaceAll("\\|\\s\\|\\|", "\\|\\|\\|");
-                        ns = ns.replaceAll("\\|\\|\\s\\|", "\\|\\|\\|");
-                        for (String ss : ns.split("\\|\\|\\|")) {
-                            percentage = (int) ((++i * 1.0) / texts.size() * 100);
-                            progressBar.setValue(percentage);
-                            label.setText(String.format("翻譯中… %d%%", percentage));
-                            newStr.add(ss);
-                        }
-                    }
-
-                    // Mapping new string with origin text id
-                    label.setText("翻譯完成，處理中...");
                     Map<String, String> newTexts = new HashMap<>();
-                    i = 0;
+
                     for (String txtId : texts.keySet()) {
-                        newTexts.put(txtId, newStr.get(i++));
+                        percentage = (int) ((++i * 1.0) / texts.size() * 100);
+                        progressBar.setValue(percentage);
+                        label.setText(String.format("翻譯中… %d%%", percentage));
+
+                        String newStr = ZhConverterUtil.toTraditional(texts.get(txtId));
+                        newTexts.put(txtId, newStr);
                     }
 
                     // Save translated texts back to Jy draft
