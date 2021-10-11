18b6fb839b45cf3c16ef7c84580daca308e3de13
Jacky Chu <jacky.ju@gmail.com>
2021-10-10 19:05:04 -0700

    fix load template json files from jar

--- src/main/java/app/jackychu/jysrttools/DraftTemplates.java
+++ src/main/java/app/jackychu/jysrttools/DraftTemplates.java
@@ -6,12 +6,7 @@ import javax.swing.*;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Locale;
-import java.util.Objects;
 
 public class DraftTemplates {
     public static final String MAC_DEFAULT_FONT_PATH = "/Applications/VideoFusion-macOS.app/Contents/Resources/Font/SystemFont/zh-hans.ttf";
@@ -20,16 +15,10 @@ public class DraftTemplates {
     public static String getTemplate(String templateName) {
         String temp = null;
         try {
-            String os = System.getProperty("os.name");
-            if (os.toLowerCase(Locale.ROOT).contains("windows")) {
-                InputStream in = DraftTemplates.class.getClassLoader().getResourceAsStream("draft_templates/" + templateName + ".json");
-                byte[] data = in.readAllBytes();
-                temp = new String(data);
-            } else {
-                temp = Files.readString(Paths.get(Objects.requireNonNull(DraftTemplates.class.getClassLoader().getResource("draft_templates/" + templateName + ".json").toURI())),
-                        StandardCharsets.UTF_8);
-            }
-        } catch (IOException | URISyntaxException | RuntimeException e) {
+            InputStream in = DraftTemplates.class.getClassLoader().getResourceAsStream("draft_templates/" + templateName + ".json");
+            byte[] data = in.readAllBytes();
+            temp = new String(data);
+        } catch (IOException | RuntimeException e) {
             JOptionPane.showMessageDialog(null,
                     new ErrorMessagePanel(e), "讀取字幕模版失敗", JOptionPane.ERROR_MESSAGE);
         }
