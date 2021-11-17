afc8ba9dfcc486a0e75aeca9d8c75b17b8f0a0da
Jacky Chu <jacky.ju@gmail.com>
2021-11-17 15:11:10 -0800

    fix subtitle edit behavior

--- src/main/java/app/jackychu/jysrttools/ui/SubtitleCellEditor.java
+++ src/main/java/app/jackychu/jysrttools/ui/SubtitleCellEditor.java
@@ -4,6 +4,11 @@ import app.jackychu.jysrttools.Subtitle;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.util.EventObject;
+import java.util.Objects;
 
 public class SubtitleCellEditor extends DefaultCellEditor {
     JTextField textField;
@@ -11,6 +16,20 @@ public class SubtitleCellEditor extends DefaultCellEditor {
     public SubtitleCellEditor() {
         super(new JTextField());
         this.textField = (JTextField) this.getComponent();
+    }
+
+    @Override
+    public boolean isCellEditable(EventObject e) {
+        if (e instanceof MouseEvent) {
+            MouseEvent me = (MouseEvent) e;
+            return me.getClickCount() >= 2;
+        } else if (e instanceof KeyEvent) {
+            KeyEvent ke = (KeyEvent) e;
+            return ke.getKeyCode() == KeyEvent.VK_ENTER;
+        } else if (e instanceof ActionEvent) {
+            return Objects.equals(((ActionEvent) e).getActionCommand(), "\n");
+        }
+        return false;
     }
 
     @Override
@@ -24,5 +43,9 @@ public class SubtitleCellEditor extends DefaultCellEditor {
                                                  Object value, boolean isSelected, int row, int column) {
         this.textField.setText(((Subtitle) value).getText());
         return this.textField;
+    }
+
+    public void focus() {
+        this.textField.grabFocus();
     }
 }
